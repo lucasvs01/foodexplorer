52ffec6ba41ed4b72019327a769e09b6ff5d7ee3
Lucas Vilaça <lucasvs04@gmail.com>
2024-07-15 11:48:55 -0300

    first commit

---
+++ src/styles/theme.js
@@ -0,0 +1,9 @@
+export default {
+    COLORS: {
+        MAIN_BACKGROUND: hsla(199, 100, 3, 100),
+        SIGN_IN_INPUT_BACKGROUND: hsla(200, 48, 10, 100),
+        BUTTON_BACKGROUND: hsla(353, 95, 24, 100)
+
+
+    }
+}
